dbddfa044ad28b40dead80516469f538360d3f07
namclu <namlu1205@gmail.com>
2017-01-14 17:07:57 +1100

    2 Add FindPodcastActivity, update layout to include a seach item and a single podcast item, update MainActivity to start FindPodcastActivity

--- app/src/main/java/com/example/namlu/podlistenerapp/MainActivity.java
+++ app/src/main/java/com/example/namlu/podlistenerapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.namlu.podlistenerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declare references to MainActivity buttons
+        TextView findPodcastActivity;
+
+        findPodcastActivity = (TextView) findViewById(R.id.find_podcast_activity_main);
+        findPodcastActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.find_podcast_activity_main) {
+            Intent findPodcastIntent = new Intent(MainActivity.this, FindPodcastActivity.class);
+            startActivity(findPodcastIntent);
+        }
     }
 }
