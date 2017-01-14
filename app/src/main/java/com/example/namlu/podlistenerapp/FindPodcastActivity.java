c9af74ff5d82240456744708f138fdf39782a422
namclu <namlu1205@gmail.com>
2017-01-14 18:10:59 +1100

    3 Update FindPodcastActivity to launch AddPodcastActivity when user taps a podcast title

--- app/src/main/java/com/example/namlu/podlistenerapp/FindPodcastActivity.java
+++ app/src/main/java/com/example/namlu/podlistenerapp/FindPodcastActivity.java
@@ -1,13 +1,27 @@
 package com.example.namlu.podlistenerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class FindPodcastActivity extends AppCompatActivity {
+public class FindPodcastActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_podcast);
+
+        // Declare references to FindPodcastActivity buttons
+        View talView = findViewById(R.id.ll_podcast_tal_activity_find_podcast);
+        talView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.ll_podcast_tal_activity_find_podcast) {
+            Intent addPodcastIntent = new Intent(FindPodcastActivity.this, AddPodcastActivity.class);
+            startActivity(addPodcastIntent);
+        }
     }
 }
