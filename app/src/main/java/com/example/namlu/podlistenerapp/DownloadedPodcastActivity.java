02a9c8aca04acabd39b8fdef7caf4a6a7d078f9e
namclu <namlu1205@gmail.com>
2017-01-14 19:27:40 +1100

    4 Add DownloadedPodcastActivity, udpate layout to include 3 podcast titles. Todo: replace images w their respective podcast images

---
+++ app/src/main/java/com/example/namlu/podlistenerapp/DownloadedPodcastActivity.java
@@ -0,0 +1,13 @@
+package com.example.namlu.podlistenerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DownloadedPodcastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downloaded_podcast);
+    }
+}
