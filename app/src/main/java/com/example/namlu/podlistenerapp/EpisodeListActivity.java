9f1e6d74ae212142c8ccf1693b652c971f19c1ac
namclu <namlu1205@gmail.com>
2017-01-15 18:35:48 +1100

    5 Add EpisodeListActivity, update layout to include one podcast title, one episode for download

---
+++ app/src/main/java/com/example/namlu/podlistenerapp/EpisodeListActivity.java
@@ -0,0 +1,13 @@
+package com.example.namlu.podlistenerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EpisodeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_episode_list);
+    }
+}
